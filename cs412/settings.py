37af5eacd0cd4886502aa33ae9478710801f8d8f
Luisa Vazquez Usabiaga <lvu@bu.edu>
2025-12-09 19:21:35 -0500

    Deployment update

--- cs412/settings.py
+++ cs412/settings.py
@@ -159,8 +159,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL= "/media/"  # note: no leading slash!
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+ 
 
 import socket
 CS_DEPLOYMENT_HOSTNAME = 'cs-webapps.bu.edu'
